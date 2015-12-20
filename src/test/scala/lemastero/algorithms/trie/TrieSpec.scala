d3daebcafbe504203546cc14f6671057b57e9de8
lenewbie <lenewbiegithub@gmail.com>
2015-12-20 17:34:26 +0100

    R-way Trie implementation without proper tests

---
+++ src/test/scala/lemastero/algorithms/trie/TrieSpec.scala
@@ -0,0 +1,19 @@
+package lemastero.algorithms.trie
+
+import lemastero.algorithms.tdd.BaseSpec
+
+
+class TrieSpec extends BaseSpec {
+
+  describe("Trie") { // TODO how on earth to test it!
+    it("works") {
+      val trie = new Trie[Integer]()
+      trie.put("sea", 42)
+
+      trie.get("sea") mustBe 42
+      trie.get("s") mustBe null
+    }
+  }
+
+
+}
