ba52a5c0dd9bcd3c3981de67dae4c2060415bb08
lemastero <lemastero@gmail.com>
2015-12-21 02:21:46 +0100

    add characterizing tests

---
+++ src/test/scala/lemastero/algorithms/trie/RWayTrieSpec.scala
@@ -0,0 +1,100 @@
+package lemastero.algorithms.trie
+
+import lemastero.algorithms.tdd.BaseSpec
+
+class RWayTrieSpec extends BaseSpec {
+
+  describe("Empty R-way Trie") {
+
+    it("has null in root and its children") {
+      val trie = new RWayTrie[Integer]()
+      trie.root.value mustBe null
+      trie.root.children.count(_ != null) mustBe 0
+    }
+
+    it("does not find any values") {
+      val trie = new RWayTrie[Integer]()
+      trie.get("foo") mustBe null
+    }
+  }
+
+  describe("put") {
+    it("with 1 character key create single node") {
+      val trie = new RWayTrie[Integer]()
+      trie.put("a", 3)
+
+      trie.root.children('a').value mustBe 3
+      trie.root.children.count(_ != null) mustBe 1
+      trie.root.children('a').children.count(_ != null) mustBe 0
+    }
+
+    it("with 3 character key create three chained node") {
+      val trie = new RWayTrie[Integer]()
+      trie.put("bar", 4)
+
+      trie.root.children('b').value mustBe null
+      trie.root.children('b').children('a').value mustBe null
+      trie.root.children('b').children('a').children('r').value mustBe 4
+
+      trie.root.children.count(_ != null) mustBe 1
+      trie.root.children('b').children.count(_ != null) mustBe 1
+      trie.root.children('b').children('a').children.count(_ != null) mustBe 1
+      trie.root.children('b').children('a').children('r').children.count(_ != null) mustBe 0
+    }
+
+    it("with 2 character key create appriopriate nodes") {
+      val trie = new RWayTrie[Integer]()
+      trie.put("ba", 12)
+      trie.put("pa", 100)
+
+      trie.root.children('b').value mustBe null
+      trie.root.children('b').children('a').value mustBe 12
+      trie.root.children('p').children('a').value mustBe 100
+      trie.root.children('p').value mustBe null
+
+      trie.root.children.count(_ != null) mustBe 2
+      trie.root.children('b').children.count(_ != null) mustBe 1
+      trie.root.children('p').children.count(_ != null) mustBe 1
+      trie.root.children('b').children('a').children.count(_ != null) mustBe 0
+      trie.root.children('p').children('a').children.count(_ != null) mustBe 0
+    }
+
+    it("with substring") {
+      val trie = new RWayTrie[Integer]()
+      trie.put("sear", 5)
+      trie.put("sea", 6)
+
+      trie.root.children('s').children('e').children('a').children('r').value mustBe 5
+      trie.root.children('s').children('e').children('a').value mustBe 6
+
+      trie.root.children('s').children('e').value mustBe null
+      trie.root.children('s').value mustBe null
+
+      trie.root.children('s').children('e').children('a').children('r').children.count(_ != null) mustBe 0
+      trie.root.children('s').children('e').children('a').children.count(_ != null) mustBe 1
+      trie.root.children('s').children('e').children.count(_ != null) mustBe 1
+      trie.root.children('s').children.count(_ != null) mustBe 1
+    }
+  }
+
+  describe("get") {
+    it("returns stored previously value") {
+      val trie = new RWayTrie[Integer]()
+      trie.put("sea", 42)
+      trie.get("sea") mustBe 42
+    }
+
+    it("does not return values that were not stored") {
+      val trie = new RWayTrie[Integer]()
+      trie.put("foobar", 42)
+
+      trie.get("fooba") mustBe null
+      trie.get("foob") mustBe null
+      trie.get("oobar") mustBe null
+      trie.get("foobaR") mustBe null
+      trie.get("Foobar") mustBe null
+    }
+  }
+
+
+}
