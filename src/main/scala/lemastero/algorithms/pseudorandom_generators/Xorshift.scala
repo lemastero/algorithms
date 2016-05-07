fd4e251e48138c394c63dca524378c9ef390c501
lemastero <lemastero@gmail.com>
2016-05-07 21:17:19 +0200

    Xorshift algorithm

---
+++ src/main/scala/lemastero/algorithms/pseudorandom_generators/Xorshift.scala
@@ -0,0 +1,18 @@
+package lemastero.algorithms.pseudorandom_generators
+
+/**
+  * Xorshift algoritym
+  * George Marsaglia, 2003
+  *
+  * https://en.wikipedia.org/wiki/Xorshift
+  * http://xoroshiro.di.unimi.it/
+  */
+case class Xorshift(seed: Int) {
+
+  def nextInt: (Int, Xorshift) = {
+    val x1 = seed ^ (seed << 21)
+    val x2 = x1 ^ (x1 >>> 35)
+    val newSeed = x2 ^ (x2 << 4)
+    (newSeed, Xorshift(newSeed))
+  }
+}
