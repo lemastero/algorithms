533d8a52776aef7b3ef5c4d24cd1dc622a9a0b29
lemastero <lemastero@gmail.com>
2016-07-18 01:48:36 +0200

    use cats Xor and Validated instead of throwing exceptions

--- src/main/scala/lemastero/algorithms/graph/path/BreadthFirstSearch.scala
+++ src/main/scala/lemastero/algorithms/graph/path/BreadthFirstSearch.scala
@@ -1,6 +1,19 @@
 package lemastero.algorithms.graph.path
 
+import cats.data.{Xor, Validated}
 import lemastero.algorithms.graph.Graph
+
+object BreadthFirstSearch {
+
+  def apply(graph: Graph, root: Int): Validated[GraphCreateError, BreadthFirstSearch] = {
+    if(graph.numberOfVertices == 0)
+      Validated.Invalid(PathFromEmptyGraph)
+    else if(root >= graph.numberOfVertices)
+      Validated.Invalid(PathFromNotExistingVertex(root, graph.numberOfVertices))
+    else Validated.Valid(new BreadthFirstSearch(graph, root))
+  }
+
+}
 
 /**
   * Breadth First Search (BFS) algorithm.
@@ -8,25 +21,23 @@ import lemastero.algorithms.graph.Graph
   * Breadth First Search differs from Depth First Search by
   * using queue to enforce processing siblings before children.
   */
-case class BreadthFirstSearch(graph: Graph, root: Int) extends PathFinder {
+class BreadthFirstSearch(graph: Graph, root: Int) extends PathFinder {
 
   private val previousVertex: Array[Option[Int]] =
     Array.fill[Option[Int]](graph.numberOfVertices)(None)
 
-  if(graph.numberOfVertices == 0) throw new PathFromEmptyGraph
-  if(root >= graph.numberOfVertices) throw new PathFromNotExistingVertex
   initialize()
 
-  override def existsPathTo(destination: Int): Boolean =
-    if (destination >= graph.numberOfVertices) throw new VertexNotFound(destination)
-    else previousVertex(destination).isDefined
+  override def existsPathTo(destination: Int): Xor[VertexNotFound, Boolean] =
+    if (destination >= graph.numberOfVertices) Xor.Left(VertexNotFound(destination))
+    else Xor.Right(previousVertex(destination).isDefined)
 
   /** Return the shortest path fro vertex provided as argument
     * to the root vertex */
-  override def getPathTo(destination: Int): List[Int] =
-    if (destination >= graph.numberOfVertices) throw VertexNotFound(destination)
-    else if (previousVertex(destination).isEmpty) List()
-    else createPathFor(destination, List[Int]())
+  override def getPathTo(destination: Int): Xor[VertexNotFound, List[Int]] =
+    if (destination >= graph.numberOfVertices) Xor.Left(VertexNotFound(destination))
+    else if (previousVertex(destination).isEmpty) Xor.Right(List())
+    else Xor.Right(createPathFor(destination, List[Int]()))
 
   private def createPathFor(destination: Int, soFar: List[Int]): List[Int] =
     if (destination == root) root :: soFar
@@ -43,7 +54,7 @@ case class BreadthFirstSearch(graph: Graph, root: Int) extends PathFinder {
 
   private def markPreviousVertices(elementsToProcess: List[Int]) {
     if (elementsToProcess.nonEmpty ) {
-      val toProcess = graph.adjacentVertices(elementsToProcess.head).filterNot(existsPathTo)
+      val toProcess = graph.adjacentVertices(elementsToProcess.head).filterNot( existsPathTo(_).getOrElse(false) )
       toProcess.foreach( previousVertex(_) = Some(elementsToProcess.head) )
       markPreviousVertices(elementsToProcess.tail ++ toProcess)
     }
